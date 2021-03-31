5fa16d43e968f4a9490cbfedac4fe95f54344145
QUARK88 <72112478+QUARK88@users.noreply.github.com>
2021-03-31 08:06:29 -0400

    Add files via upload

---
+++ ideologies.js
@@ -0,0 +1,122 @@
+ideologies = [
+    {
+        "name": "Centrism",
+        "stats": {
+            "bregu": 50,
+            "bprop": 50,
+            "brepa": 50,
+            "btrad": 50,
+            "bwork": 50,
+            "bcont": 50,
+            "bindu": 50,
+        }
+    },
+    {
+        "name": "Quarkism",
+        "stats": {
+            "bregu": 80,
+            "bprop": 55,
+            "brepa": 35,
+            "btrad": 90,
+            "bwork": 65,
+            "bcont": 65,
+            "bindu": 30,
+        }
+    },
+    {
+        "name": "Anarcho Capitalism",
+        "stats": {
+            "bregu": 0,
+            "bprop": 0,
+            "brepa": 0,
+            "btrad": 100,
+            "bwork": 0,
+            "bcont": 0,
+            "bindu": 0,
+        }
+    },
+    {
+        "name": "Marxism Leninism",
+        "stats": {
+            "bregu": 90,
+            "bprop": 100,
+            "brepa": 80,
+            "btrad": 65,
+            "bwork": 80,
+            "bcont": 90,
+            "bindu": 25,
+        }
+    },
+    {
+        "name": "Market Socialism",
+        "stats": {
+            "bregu": 20,
+            "bprop": 100,
+            "brepa": 50,
+            "btrad": 25,
+            "bwork": 90,
+            "bcont": 20,
+            "bindu": 50,
+        }
+    },
+    {
+        "name": "Fascist Corporatism",
+        "stats": {
+            "bregu": 80,
+            "bprop": 35,
+            "brepa": 25,
+            "btrad": 90,
+            "bwork": 30,
+            "bcont": 90,
+            "bindu": 20,
+        }
+    },
+    {
+        "name": "Neoliberalism",
+        "stats": {
+            "bregu": 40,
+            "bprop": 15,
+            "brepa": 25,
+            "btrad": 10,
+            "bwork": 40,
+            "bcont": 50,
+            "bindu": 40,
+        }
+    },
+    {
+        "name": "Social Democracy",
+        "stats": {
+            "bregu": 65,
+            "bprop": 65,
+            "brepa": 50,
+            "btrad": 30,
+            "bwork": 60,
+            "bcont": 60,
+            "bindu": 50,
+        }
+    },
+    {
+        "name": "Adam Smith model",
+        "stats": {
+            "bregu": 20,
+            "bprop": 10,
+            "brepa": 20,
+            "btrad": 10,
+            "bwork": 50,
+            "bcont": 0,
+            "bindu": 25,
+        }
+    },
+    {
+        "name": "Anarcho Communalism",
+        "stats": {
+            "bregu": 0,
+            "bprop": 100,
+            "brepa": 100,
+            "btrad": 50,
+            "bwork": 100,
+            "bcont": 0,
+            "bindu": 80,
+        }
+    }
+];
